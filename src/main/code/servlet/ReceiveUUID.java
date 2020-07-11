eb48095b5e1737dbb4369141a8386a9ffe5f909d
shuyangxiaobao <825065886@qq.com>
2020-07-11 17:42:00 +0800

    自动签名

--- src/main/code/servlet/ReceiveUUID.java
+++ src/main/code/servlet/ReceiveUUID.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,30 +13,28 @@ import java.util.UUID;
 
 
 /**
- *
  * Cookie快速入门
- *
  */
 
 @WebServlet("/receiveUUID")
 public class ReceiveUUID extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
+        response.setContentType ( "text/html;charset=UTF-8" );
+        request.setCharacterEncoding ( "UTF-8" );
         //获取HTTP请求的输入流
-        InputStream is = request.getInputStream();
+        InputStream is = request.getInputStream ();
         //已HTTP请求输入流建立一个BufferedReader对象
-        BufferedReader br = new BufferedReader(new InputStreamReader (is,"UTF-8"));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader ( new InputStreamReader ( is, "UTF-8" ) );
+        StringBuilder sb = new StringBuilder ();
 
         //读取HTTP请求内容
         String buffer = null;
-        while ((buffer = br.readLine()) != null) {
-            sb.append(buffer);
+        while ((buffer = br.readLine ()) != null) {
+            sb.append ( buffer );
         }
         String content = "";
-        if (sb.length () > 0){
-             content = sb.toString().substring(sb.toString().indexOf("<?xml"), sb.toString().indexOf("</plist>")+8);
+        if (sb.length () > 0) {
+            content = sb.toString ().substring ( sb.toString ().indexOf ( "<?xml" ), sb.toString ().indexOf ( "</plist>" ) + 8 );
         }
         //content就是接收到的xml字符串
         //进行xml解析即可
@@ -41,38 +42,66 @@ public class ReceiveUUID extends HttpServlet {
 
         int i = content.indexOf ( "<key>UDID</key>\t<string>" );
         int j = content.indexOf ( "</string>\t<key>VERSION" );
-        udid = content.substring ( i,j );
-
-        System.out.printf ( "😄1"+udid +"1😄");
+        udid = content.substring ( i, j );
 
         udid = udid.substring ( 24 );
-        System.out.printf ( "😄2"+udid +"2😄");
-
-        FileOutputStream fos = new FileOutputStream("updata_profile/multiple-device-upload-ios.txt",true);
-        for (int k = 1; k <=10 ; k++) {
-            fos.write( udid.getBytes());
-            String name = "\t"+System.currentTimeMillis () +"\n";
-            fos.write(name.getBytes());
-        }
-        fos.close();
+        System.out.println ( "😄" + udid + "😄" );
 
 
-
-        response.setStatus(301); //301之后iOS设备会自动打开safari浏览器
-        response.setHeader("Location", "https://shuyangxiaobao.github.io/ioschaojiqianming?UDID="+udid);
+        response.setStatus ( 301 ); //301之后iOS设备会自动打开safari浏览器
+        response.setHeader ( "Location", "https://shuyangxiaobao.github.io/ioschaojiqianming?UDID=" + udid );
         //http://192.168.1.106:8080/udid.jsp 是用于显示udid的页面,也可以利用之前的下载mobileprofile文件页面
+
+        FileOutputStream fos = new FileOutputStream ( "1111.txt", false );
+        for (int k = 1; k <= 1; k++) {
+            fos.write ( udid.getBytes () );
+            String name = "\t" + System.currentTimeMillis () + "\n";
+            fos.write ( name.getBytes () );
+        }
+        fos.close ();
+        File file = new File ( this.getClass ().getResource ( "/" ).getPath () );
+
+        String rbpath = "/Users/xiaobao/java/Tomcat/apache-tomcat-8.5.51/webapps/ROOT/UpdateProfile.rb";
+
+        try {
+
+            Process p = Runtime.getRuntime().exec("ruby "+rbpath+" true");
+            p.waitFor();
+            System.out.println(p.exitValue());
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+        }
+
+        String signPath = "/Users/xiaobao/java/Tomcat/apache-tomcat-8.5.51/webapps/ROOT/qianming.sh";
+        try {
+
+            Process p = Runtime.getRuntime().exec("sh "+signPath);
+            p.waitFor();
+            System.out.println(p.exitValue());
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
 
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.doPost(request, response);
+        this.doPost ( request, response );
     }
 }
 
-//<key>UDID</key>	<string>6399f81e6998772826ddad8ded01cb448d4faedc</string>	<key>VERSION</key>
-//
-//<key>UDID</key>
-//
-//    <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict>	<key>IMEI</key>	<string>35 534408 429420 9</string>	<key>PRODUCT</key>	<string>iPhone9,1</string>	<key>SERIAL</key>	<string>DNQT1F84HG74</string>	<key>UDID</key>	<string>6399f81e6998772826ddad8ded01cb448d4faedc</string>	<key>VERSION</key>	<string>17F80</string></dict></plist>
-//content
+
+
+//<string>https://shuyangxiaobao.github.io/ioschaojiqianming/src/main/webapp/xlp.ipa</string>
---
+++ target/classes/servlet/UpdateProfile.rb
@@ -0,0 +1,46 @@
+require "spaceship"
+
+Spaceship.login('MP@quantdo.com.cn', 'Quantdo123456')
+
+#参数传入true表示需要新增设备，例如：ruby UpdateProfile.rb true
+if ARGV[0] == "true"
+    file = File.open("1111.txt") #文本文件里录入的udid和设备名用tab分隔
+    puts "Updating #{p}"
+
+    file.each do |line|
+        arr = line.strip.split("\t")
+        device = Spaceship.device.create!(name: arr[1], udid: arr[0])
+        puts "add device: #{device.name} #{device.udid} #{device.model}"
+    end
+
+    devices = Spaceship.device.all
+    puts "length:: #{devices.length}"
+
+    profiles = Array.new
+    profiles += Spaceship.provisioning_profile.development.all
+#    profiles += Spaceship.provisioning_profile.ad_hoc.all
+
+    profiles.each do |p|
+        puts "Updating #{p.name}"
+        p.devices = devices
+        p.update!
+    end
+end
+
+dic = {"qdinfini.com.cn.dev" => "qdinfini.com.cn.dev.mobileprovision",
+    "qdinfini.com.cn2.dev" => "qdinfini.com.cn2.dev.mobileprovision",
+    "qdinfini.com.cn2.NotificationService.dev" => "qdinfini.com.cn2.NotificationService.dev.mobileprovision",
+    "qdinfini.com.NotificationService.dev" => "qdinfini.com.NotificationService.dev.mobileprovision",
+    "wuchanjinshu.dev" => "wuchanjinshu.dev.mobileprovision"
+}
+
+downloadProfiles = Array.new
+downloadProfiles += Spaceship.provisioning_profile.development.all
+#downloadProfiles += Spaceship.provisioning_profile.ad_hoc.all
+
+downloadProfiles.each do |p|
+    puts "Hello, Ruby!";
+#    puts "Downloading #{p.name}"
+    fileName = dic[p.name]
+    File.write("/Users/xiaobao/Library/MobileDevice/Provisioning Profiles/#{p.name}.mobileprovision", p.download)
+end
